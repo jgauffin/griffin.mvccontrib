8959f330d426cc2a3e8606132c2386f7b9eba923
Jonas Gauffin <jonas@gauffin.org>
2012-02-09 15:28:55 +0100

    Fixes for sql server

--- source/Griffin.MvcContrib.SqlServer.Tests/LocalizedTypesRepositoryTests.cs
+++ source/Griffin.MvcContrib.SqlServer.Tests/LocalizedTypesRepositoryTests.cs
@@ -5,52 +5,70 @@ using Xunit;
 
 namespace Griffin.MvcContrib.SqlServer.Tests
 {
-	public class LocalizedTypesRepositoryTests
-	{
-		private SqlLocalizedTypesRepository _repository = new SqlLocalizedTypesRepository(new ConnectionFactory());
+    public class LocalizedTypesRepositoryTests
+    {
+        private ConnectionFactory _factory = new ConnectionFactory();
+        private SqlLocalizedTypesRepository _repository ;
 
-		[Fact]
-		public void GetNonExistant()
-		{
-			var p = _repository.GetPrompt(new CultureInfo(1053), new TypePromptKey(typeof (TestType), "FirstNameOrSomething"));
-			Assert.Null(p);
-		}
+        public LocalizedTypesRepositoryTests()
+        {
+            _repository = new SqlLocalizedTypesRepository(_factory);
+        }
 
-		[Fact]
-		public void GetExisting()
-		{
-			var key = new TypePromptKey(typeof (TestType), "FirstName");
-			_repository.Save(new CultureInfo(1053),  typeof(TestType), "FirstName", "Förnamn");
+        [Fact]
+        public void GetNonExistant()
+        {
+            var p = _repository.GetPrompt(new CultureInfo(1053), new TypePromptKey(typeof (TestType), "FirstNameOrSomething"));
+            Assert.Null(p);
+        }
 
-			var prompt = _repository.GetPrompt(new CultureInfo(1053), key);
-			Assert.NotNull(prompt);
-			Assert.Equal("Förnamn", prompt.TranslatedText);
-		}
+        [Fact]
+        public void TestCreateLang()
+        {
+            using (var cmd = _factory.Connection.CreateCommand())
+            {
+                cmd.CommandText = "DELETE FROM LocalizedTypes WHERE LocaleId = 1044";
+                cmd.ExecuteNonQuery();
+            }
 
-		[Fact]
-		public void Update()
-		{
-			var key = new TypePromptKey(typeof(TestType), "FirstName");
-			_repository.Update(new CultureInfo(1053), key, "Förrenammne");
-			var prompt = _repository.GetPrompt(new CultureInfo(1053), key);
-			Assert.NotNull(prompt);
-			Assert.Equal("Förrenammne", prompt.TranslatedText);
-		}
+            _repository.CreateLanguage(new CultureInfo(1044), new CultureInfo(1033));
+        }
 
-		[Fact]
-		public void TwoLanguages()
-		{
-			var key = new TypePromptKey(typeof(TestType), "FirstName");
-			_repository.Save(new CultureInfo(1033), typeof(TestType), "FirstName", "FirstName");
-			_repository.Save(new CultureInfo(1053), typeof(TestType), "FirstName", "Förnamn");
+        [Fact]
+        public void GetExisting()
+        {
+            var key = new TypePromptKey(typeof (TestType), "FirstName");
+            _repository.Save(new CultureInfo(1053),  typeof(TestType), "FirstName", "Förnamn");
+
+            var prompt = _repository.GetPrompt(new CultureInfo(1053), key);
+            Assert.NotNull(prompt);
+            Assert.Equal("Förnamn", prompt.TranslatedText);
+        }
+
+        [Fact]
+        public void Update()
+        {
+            var key = new TypePromptKey(typeof(TestType), "FirstName");
+            _repository.Update(new CultureInfo(1053), key, "Förrenammne");
+            var prompt = _repository.GetPrompt(new CultureInfo(1053), key);
+            Assert.NotNull(prompt);
+            Assert.Equal("Förrenammne", prompt.TranslatedText);
+        }
+
+        [Fact]
+        public void TwoLanguages()
+        {
+            var key = new TypePromptKey(typeof(TestType), "FirstName");
+            _repository.Save(new CultureInfo(1033), typeof(TestType), "FirstName", "FirstName");
+            _repository.Save(new CultureInfo(1053), typeof(TestType), "FirstName", "Förnamn");
 
 
-			var enprompt = _repository.GetPrompt(new CultureInfo(1033), key);
-			var seprompt = _repository.GetPrompt(new CultureInfo(1053), key);
-			Assert.NotNull(enprompt);
-			Assert.NotNull(seprompt);
-			Assert.NotEqual(enprompt.TranslatedText, seprompt.TranslatedText);
-		}
+            var enprompt = _repository.GetPrompt(new CultureInfo(1033), key);
+            var seprompt = _repository.GetPrompt(new CultureInfo(1053), key);
+            Assert.NotNull(enprompt);
+            Assert.NotNull(seprompt);
+            Assert.NotEqual(enprompt.TranslatedText, seprompt.TranslatedText);
+        }
 
-	}
+    }
 }
